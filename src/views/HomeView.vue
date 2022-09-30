53301f3d3d4fc5ea62f75cb1ec8ea953d92452b2
Alex <ag.perez0500@gmail.com>
2022-09-30 13:01:08 -0500

    able to see prodcuts from the backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,75 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Trivia
+        </p>
+        <p class="subtitle">
+          Random Trivias
+        </p>
+      </div>
+    </section>
+    
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Trivias</h2>
+      </div>
+
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail">
+          </figure>
+          <h3 class="is-size-4"> {{product.name}}</h3>
+          <p class="is-size-6 has-text-grey">${{product.price}}</p>
+
+          View Details
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .image{
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>

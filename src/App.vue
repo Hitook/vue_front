0f49ff80cc5d6e63a91c79ee2a832b26e59d682b
Alex <ag.perez0500@gmail.com>
2022-10-01 15:38:33 -0500

    I can search for trivias

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,26 @@
         </a>
       </div>
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu }">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="Search a category" name="query">
+                </div>
+                <div class="control">
+                  <button class="button is-success">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+
+                </div>
+              </div>
+            </form>
+
+          </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/history" class="navbar-item">History</router-link>
           <router-link to="/computer_science" class="navbar-item">Computer Science</router-link>

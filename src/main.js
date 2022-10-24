77ae77d38c0154cfd887f0b31fe5fc18881070a6
Alex <47337720+Hitook@users.noreply.github.com>
2022-10-24 16:53:15 -0500

    fixed hopefully

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueSession from "vue-session"
 import router from './router'
 import store from './store'
 import axios from 'axios'
 
 axios.defaults.baseURL = "http://44.203.134.65"
 
-//Vue.use(VueSession);
-//Vue.config.productionTip = false;
 createApp(App).use(store, VueSession).use(router, axios).mount('#app')
